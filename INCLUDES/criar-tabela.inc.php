19669f8eda986733371e7914d0997f099d3da2ad
robert-barros <robertbarbosabarros@gmail.com>
2025-02-26 22:33:06 -0300

    adicionando logica de funcionario vendas e login

--- INCLUDES/criar-tabela.inc.php
+++ INCLUDES/criar-tabela.inc.php
@@ -8,3 +8,18 @@ $sql ="CREATE TABLE IF NOT EXISTS $NomeDaTabela1(
     DATA TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )ENGINE = InnoDB";
 $conexao->query($sql) OR exit($conexao->error);
+
+
+
+$sql = "CREATE TABLE IF NOT EXISTS  Venda (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    funcionario_id INT NOT NULL,
+    servico_id INT NOT NULL,
+    data_venda DATETIME DEFAULT CURRENT_TIMESTAMP,
+    observacoes TEXT,
+    FOREIGN KEY (cliente_id) REFERENCES Cliente(id),
+    FOREIGN KEY (funcionario_id) REFERENCES Funcionario(id),
+    FOREIGN KEY (servico_id) REFERENCES Servico(id)
+)ENGINE = InnoDB";
+$conexao->query($sql) OR exit($conexao->error);
