d81db89c11b3d340aa4b49246adfe31c9f82cc30
robert-barros <robertbarbosabarros@gmail.com>
2025-02-27 02:14:47 -0300

    atualizacao de login

--- INCLUDES/criar-tabela.inc.php
+++ INCLUDES/criar-tabela.inc.php
@@ -10,7 +10,6 @@ $sql ="CREATE TABLE IF NOT EXISTS $NomeDaTabela1(
 $conexao->query($sql) OR exit($conexao->error);
 
 
-
 $sql = "CREATE TABLE IF NOT EXISTS  Venda (
     id INT AUTO_INCREMENT PRIMARY KEY,
     cliente_id INT NOT NULL,
@@ -23,3 +22,20 @@ $sql = "CREATE TABLE IF NOT EXISTS  Venda (
     FOREIGN KEY (servico_id) REFERENCES Servico(id)
 )ENGINE = InnoDB";
 $conexao->query($sql) OR exit($conexao->error);
+
+
+$sql = "CREATE TABLE funcionarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    telefone VARCHAR(15),
+    endereco VARCHAR(150),
+    numero VARCHAR(10),
+    bairro VARCHAR(50),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    funcao VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50),
+    senha VARCHAR(255) NOT NULL
+)ENGINE = InnoDB";
+$conexao->query($sql) OR exit($conexao->error);
+
